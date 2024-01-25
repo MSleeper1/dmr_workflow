8a7d0428485283cfbb662c663ed5bcdba23c657d
MSleeper1 <sleeper.meghan@gmail.com>
2024-01-25 11:58:46 -0800

    prep-steps split to subworkflow, working status

--- src/0_index-ref-genome-bismark.sh
+++ src/0_index-ref-genome-bismark.sh
@@ -5,29 +5,61 @@
 #SBATCH -N 1                                    # number of nodes           
 #SBATCH -n 1                                    # number of cores
 #SBATCH -c 4                                    # number of cpus per task
-#SBATCH --mem=18000                             # memory pool to all cores
+#SBATCH --mem=15gb                              # memory pool to all cores
 #SBATCH -e slurm.index.bismark.j%j.err          # STANDARD ERROR FILE TO WRITE TO
 #SBATCH -o slurm.index.bismark.j%j.out          # STANDARD OUTPUT FILE TO WRITE TO
 #SBATCH --mail-user=msleeper@ucdavis.edu        # YOUR EMAIL ADDRESS
 #SBATCH --mail-type=ALL                         # NOTIFICATIONS OF SLURM JOB STATUS 
 
+'''
+------------------------
+ description and usage
+------------------------
+0_index-ref-genome-bismark.sh
+Created: 1/24/24 MS
 
-#-----------------------------------------------------------------------------------#
-# description and usage
-#-----------------------------------------------------------------------------------#
-# 0_index-ref-genome.sh
-# Created: 1/24/24 MS
-#
-# this script will index a reference genome for alignment with bismark
-#
-# input reference genome
-#
-# options:
-#   -r indicates the directory containing reference genome to use (use full path to directory)
-# does not take positional arguments
-#
-# sbatch 0_index-ref-genome-bismark.sh -r [reference-genome]
-#-----------------------------------------------------------------------------------#
+this script will index a reference genome for alignment with bismark
+
+input directory containing reference genome in fasta.gz format:
+  - example input:
+    - /home/user/genomes/human/hg38/GRCh38_no_alt_analysis_set/
+    - specified location must contain reference fasta file (ex: "GRCh38_no_alt_analysis_set.fa.gz")
+
+output is a directory of index files:
+  - bismark default output directory is named "Bisulfite_Genome"
+  - 3 directories, 14 files
+  - structure:
+        Bisulfite_Genome/
+        ├── CT_conversion
+        │   ├── BS_CT.1.bt2
+        │   ├── BS_CT.2.bt2
+        │   ├── BS_CT.3.bt2
+        │   ├── BS_CT.4.bt2
+        │   ├── BS_CT.rev.1.bt2
+        │   ├── BS_CT.rev.2.bt2
+        │   └── genome_mfa.CT_conversion.fa
+        └── GA_conversion
+            ├── BS_GA.1.bt2
+            ├── BS_GA.2.bt2
+            ├── BS_GA.3.bt2
+            ├── BS_GA.4.bt2
+            ├── BS_GA.rev.1.bt2
+            ├── BS_GA.rev.2.bt2
+            └── genome_mfa.GA_conversion.fa
+
+options:
+  -r indicates the directory containing reference genome to use (use full path to directory)
+  - does not take positional arguments
+
+usage:
+sbatch 0_index-ref-genome-bismark.sh -r [reference-genome]
+
+resources used:
+indexing human hg38 analysis set (GRCh38_no_alt_analysis_set.fa.gz)
+  - time: 3 hours
+  - memory: 11 gb
+-------------------------------------------
+'''
 
 # activate conda in general
 . "/home/msleeper/miniconda3/etc/profile.d/conda.sh"
